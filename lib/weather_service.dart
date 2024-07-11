4879b1e3c69a68b9de343100a97564c7fd1d3ec2
Subaina Usman <subainausman955@gmail.com>
2024-07-11 17:01:53 +0500

    ..
    ..

--- lib/weather_service.dart
+++ lib/weather_service.dart
@@ -3,10 +3,9 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class WeatherService {
-  final String apiKey = "e640ac88c49641d38b275733242505";
+  final String apiKey = "your api key";
 
-  final String forecastBaseUrl =
-      'http://api.weatherapi.com/v1/forecast.json';
+  final String forecastBaseUrl = 'http://api.weatherapi.com/v1/forecast.json';
   final String searchBaseUrl = 'http://api.weatherapi.com/v1/search.json';
 
   Future<Map<String, dynamic>> fetchCurrentWeather(String city) async {
@@ -19,6 +18,7 @@ class WeatherService {
       throw Exception('Failed to load weather data');
     }
   }
+
   Future<Map<String, dynamic>> fetch7DayForecast(String city) async {
     final url = '$forecastBaseUrl?key=$apiKey&q=$city&days=7&aqi=no&alerts=no';
 
@@ -29,6 +29,7 @@ class WeatherService {
       throw Exception('Failed to load forecast data');
     }
   }
+
   Future<List<dynamic>?> fetchCitySuggestions(String query) async {
     final url = '$searchBaseUrl?key=$apiKey&q=$query';
 
